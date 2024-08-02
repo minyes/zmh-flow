1ffbc7fe5cc729e66c81f145bd451d6b2f16465d
zhouminghao <467914486@qq.com>
2024-08-02 23:22:46 +0800

    DDD模型项目结构搭建 添加配置

---
+++ zmh-flow-app/src/main/java/com/zmh/flow/app/StartApplication.java
@@ -0,0 +1,13 @@
+package com.zmh.flow.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
